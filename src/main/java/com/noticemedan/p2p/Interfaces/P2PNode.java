56614649b3d9232e18ea66551c2d99def957914c
alol <alol@itu.dk>
2018-11-05 11:07:15 +0100

    Created base-interface for p2p node

--- src/main/java/com/noticemedan/p2p/Interfaces/P2PNode.java
+++ src/main/java/com/noticemedan/p2p/Interfaces/P2PNode.java
@@ -1,4 +1,17 @@
 package com.noticemedan.p2p.Interfaces;
 
+
 public interface P2PNode {
+
+    void addValue(int key, String value);
+
+    //Takes a key and returns a value from an internal hashtable;
+    String sendValue(int key);
+
+    void sendBackup();
+
+    void assignPartner();
+
+    void healthCheck();
+
 }
--- src/main/java/com/noticemedan/p2p/P2PNode.java
+++ src/main/java/com/noticemedan/p2p/P2PNode.java
@@ -1,6 +1,6 @@
 package com.noticemedan.p2p;
 
-public class P2PNode {
+public class P2PNode{
 	public static void main(String[] args) {
 		System.out.println("Hi mom");
 	}
